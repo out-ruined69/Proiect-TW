bfbb5a8d2c958584b6a78745d20bcc0ddc12db8d
out-ruined69 <andrei-alexandru.harnagea@s.unibuc.ro>
2022-05-14 01:57:58 +0300

    proiect

---
+++ code.js
@@ -0,0 +1,10 @@
+function randomInterval(x, y) {
+    return Math.floor(Math.random() * (y - x + 1) + x)
+  }
+  
+  var rnd=1;
+      setInterval(function(){
+        const rnd = randomInterval(1, 4);
+        document.getElementById('bt' + rnd).checked = true;
+      }, 5000);
+  
