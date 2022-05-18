b2872bef0567cf5c06b6c4834c28da99ee8b8ac8
out-ruined69 <andrei-alexandru.harnagea@s.unibuc.ro>
2022-05-18 23:19:20 +0300

    .

--- code_about.js
+++ code_about.js
@@ -26,3 +26,22 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+document.querySelector("#fisier").addEventListener("change", function(){
+  const reader = new FileReader();
+
+reader.addEventListener("load", () => {
+  localStorage.setItem("recent-image", reader.result);
+});
+
+  reader.readAsDataURL(this.files[0]);
+});
+
+document.addEventListener("DOMContentLoaded", () =>{
+    const recentImageDataUrl = localStorage.getItem("recent-image");
+
+    if(recentImageDataUrl){
+      document.querySelector("#imgPreview").setAttribute("src", recentImageDataUrl);
+    }
+
+});
